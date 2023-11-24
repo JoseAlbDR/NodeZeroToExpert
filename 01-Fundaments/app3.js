c02ccdc87275f0bd8b31d568a4f25c71bfa453a6
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-24 22:30:53 +0100

    feat: react word count

--- 01-Fundaments/app3.js
+++ 01-Fundaments/app3.js
@@ -5,3 +5,9 @@ const content = fs.readFileSync('README.md', 'utf-8');
 const workCount = content.split(' ');
 
 console.log('Words:', workCount.length);
+
+const reactWords = workCount.filter((word) =>
+  word.toLowerCase().includes('react')
+);
+
+console.log('React:', reactWords.length);
