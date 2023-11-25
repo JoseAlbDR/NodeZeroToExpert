7ce2ad478710bf3cebdd9c8b0426146545f896c8
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-25 17:01:53 +0100

    feat: hhtp-client plugin (adapter patron)

---
+++ 02-Bases/src/plugins/http-client.plugin.js
@@ -0,0 +1,9 @@
+const httpClientPlugin = {
+  get: async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  },
+};
+
+module.exports = { httpClientPlugin };
