611126c30a4140c919b101ba57ea524336d8e0fa
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-25 13:18:37 +0100

    refactor: getUUID adapter patron refactor uuid package

--- 02-Bases/src/js-foundation/04-factory.js
+++ 02-Bases/src/js-foundation/04-factory.js
@@ -1,11 +1,11 @@
-const { v4: uuidv4 } = require('uuid');
-const { getAge } = require('../public/get-age.plugin');
+const { getAge } = require('../plugins/get-age.plugin');
+const { getUUID } = require('../plugins/get-id.plugin');
 
 const obj = { name: 'John', birthdate: '02-07-1984' };
 
 const buildPerson = ({ name, birthdate }) => {
   return {
-    id: uuidv4(),
+    id: getUUID(),
     name,
     birthdate,
     age: getAge(birthdate),
