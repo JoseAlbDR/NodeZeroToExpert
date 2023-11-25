4d53770afbcb201ab7f8150fbd113c3687477bb9
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-25 17:03:25 +0100

    feat: post, put, delete in httpClientPlugin

--- 02-Bases/src/plugins/http-client.plugin.js
+++ 02-Bases/src/plugins/http-client.plugin.js
@@ -4,6 +4,9 @@ const httpClientPlugin = {
     const data = await response.json();
     return data;
   },
+  post: async (url, data) => {},
+  put: async (url, data) => {},
+  delete: async (url) => {},
 };
 
 module.exports = { httpClientPlugin };
