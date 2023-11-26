d7b8e0f2c2fb202a6606da1712c6f9736656bf1c
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-26 09:21:07 +0100

    feat: 03-callbacks tests complete

--- 02-Bases/tests/js-foundation/03-callbacks.test.ts
+++ 02-Bases/tests/js-foundation/03-callbacks.test.ts
@@ -2,12 +2,26 @@ import 'jest';
 import { getUserById } from '../../src/js-foundation/03-callbacks';
 
 describe('js-foundation/03-callbacks.ts', () => {
-  test('getUserById sholud return an error if user do not exist', (done) => {
+  test('getUserById should return an error if user do not exist', (done) => {
     const id = 10;
 
     getUserById(id, (err, user) => {
       expect(err).toBe(`User not found with id ${id}`);
       expect(user).toBeUndefined();
+
+      done();
+    });
+  });
+
+  test('getUserById should return John Doe', (done) => {
+    const id = 1;
+
+    getUserById(id, (err, user) => {
+      expect(err).toBeNull();
+      expect(user).toStrictEqual({
+        id: 1,
+        name: 'John Doe',
+      });
 
       done();
     });
