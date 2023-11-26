6e2831506a70a0fb762f7e6c2ab920161010bfa6
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-26 19:44:16 +0100

    feat: logger tests

---
+++ 02-Bases/tests/plugins/logger.test.ts
@@ -0,0 +1,34 @@
+import 'jest';
+import { buildLogger } from '../../src/plugins';
+import { logger } from '../../src/plugins/logger.plugin';
+
+describe('plugins/logger.plugin.ts', () => {
+  test('buildLogger should return a function logger that have log and error methods', () => {
+    const service = 'test';
+
+    const logger = buildLogger(service);
+
+    expect(typeof logger).toBe('object');
+    expect(typeof logger.log).toBe('function');
+    expect(typeof logger.error).toBe('function');
+  });
+
+  test('logger.log should log a message', () => {
+    const winstonLoggerMock = jest.spyOn(logger, 'log');
+    const message = 'test message';
+    const service = 'test service';
+
+    const customLogger = buildLogger(service);
+
+    customLogger.log(message);
+
+    expect(winstonLoggerMock).toHaveBeenCalledWith(
+      'info',
+      expect.objectContaining({
+        level: 'info',
+        message,
+        service,
+      })
+    );
+  });
+});
