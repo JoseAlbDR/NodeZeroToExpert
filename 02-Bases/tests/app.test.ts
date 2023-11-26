747517d1677cc2ce4af71c5aa494c131b7d3f318
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-27 00:41:00 +0100

    feat: initial setup complete

--- 02-Bases/tests/app.test.ts
+++ 02-Bases/tests/app.test.ts
@@ -1,16 +1,16 @@
-// import { describe, expect, it, test } from '@jest/globals';
-import 'jest';
+// // import { describe, expect, it, test } from '@jest/globals';
+// import 'jest';
 
-describe('Test in the App File', () => {
-  test('should be 30', () => {
-    // 1. Arrange
-    const num1 = 10;
-    const num2 = 20;
+// describe('Test in the App File', () => {
+//   test('should be 30', () => {
+//     // 1. Arrange
+//     const num1 = 10;
+//     const num2 = 20;
 
-    // 2. Act
-    const result = num1 + num2;
+//     // 2. Act
+//     const result = num1 + num2;
 
-    // 3. Assert
-    expect(result).toBe(30);
-  });
-});
+//     // 3. Assert
+//     expect(result).toBe(30);
+//   });
+// });
