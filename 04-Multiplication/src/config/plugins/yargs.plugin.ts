60682d161aac801599e1544a476bda787486b6e5
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-27 12:55:47 +0100

    feat: required option b, base and optional option l, limit (default 10)

--- 04-Multiplication/src/config/plugins/yargs.plugin.ts
+++ 04-Multiplication/src/config/plugins/yargs.plugin.ts
@@ -1,4 +1,17 @@
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
-export const yarg = yargs(hideBin(process.argv)).parseSync();
+export const yarg = yargs(hideBin(process.argv))
+  .option('b', {
+    alias: 'base',
+    type: 'number',
+    demandOption: true,
+    describe: 'Multiplication table base',
+  })
+  .option('l', {
+    alias: 'limit',
+    type: 'number',
+    default: 10,
+    describe: 'Multiplication table limit',
+  })
+  .parseSync();
