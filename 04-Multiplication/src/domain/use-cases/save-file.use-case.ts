a800d76dbd5f0b1a8f9cdd3f2179e8e3daa00fb8
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-27 17:33:54 +0100

    feat: SaveFile use case

---
+++ 04-Multiplication/src/domain/use-cases/save-file.use-case.ts
@@ -0,0 +1,33 @@
+import fs from 'fs';
+import path from 'path';
+export interface SaveFileUseCase {
+  execute: (options: SaveFileOptions) => boolean;
+}
+
+export interface SaveFileOptions {
+  fileContent: string;
+  destination?: string;
+  fileName?: string;
+}
+
+export class SaveFile implements SaveFileUseCase {
+  constructor() {} // repository: StorageRepository
+
+  execute({
+    fileContent,
+    destination = 'outputs',
+    fileName = 'table.txt',
+  }: SaveFileOptions): boolean {
+    try {
+      if (!fs.existsSync(path.join(destination))) {
+        fs.mkdirSync(path.join(destination), { recursive: true });
+      }
+      fs.writeFileSync(`${destination}/${fileName}`, fileContent);
+      console.log('File created!');
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
+}
