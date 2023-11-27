d38946e89ffaab7c0e6709541b2665b15dc7fd5b
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-27 01:02:45 +0100

    feat: script para crear una tabla de multiplicar

--- 04-Multiplication/src/app.ts
+++ 04-Multiplication/src/app.ts
@@ -1,3 +1,24 @@
-const message = 'Hello world!!!';
+import fs from 'fs';
+import path from 'path';
 
-console.log(message);
+const arrayTabla = [];
+const base = 5;
+
+for (let index = 0; index < 10; index++) {
+  const row = `${base} x ${index + 1} = ${base * (index + 1)}`;
+  arrayTabla.push(row);
+}
+
+const header = `
+===============================
+\t Tabla del ${base}
+===============================\n
+`;
+
+const output = header + arrayTabla.join('\n');
+
+if (!fs.existsSync(path.join('outputs'))) {
+  fs.mkdirSync(path.join('outputs'));
+}
+
+fs.writeFileSync(`outputs/tabla-${base}.txt`, output);
