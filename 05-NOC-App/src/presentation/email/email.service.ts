162fc9977272214d2fb4c4187eae021f8823b640
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-02 22:51:12 +0100

    feat: send email logs usecase

--- 05-NOC-App/src/presentation/email/email.service.ts
+++ 05-NOC-App/src/presentation/email/email.service.ts
@@ -1,14 +1,19 @@
 import nodemailer from 'nodemailer';
 import { envs } from '../../config/plugins/env.plugin';
+import { LogRepository } from '../../domain/repository/log.repository';
+import { LogEntity, LogSeverityLevel } from '../../domain/entities/log.entity';
 
 interface SendMailOptions {
-  to: string;
+  to: string | string[];
   subject: string;
   htmlBody: string;
-  //TODO attachements:
+  attachments: Attachment[];
 }
 
-// TODO attachements
+interface Attachment {
+  filename: string;
+  path: string;
+}
 
 export class EmailService {
   private transporter = nodemailer.createTransport({
@@ -19,19 +24,44 @@ export class EmailService {
     },
   });
 
+  constructor() {}
+
   async sendEmail(options: SendMailOptions): Promise<boolean> {
-    const { to, subject, htmlBody } = options;
+    const { to, subject, htmlBody, attachments = [] } = options;
 
     try {
       const sentInformation = await this.transporter.sendMail({
         to,
         subject,
+        attachments,
         html: htmlBody,
       });
-      console.log(sentInformation);
+      // console.log(sentInformation)
+
       return true;
     } catch (error) {
       return false;
     }
+  }
+
+  sendEmailWithFileSystemLogs(to: string | string[]) {
+    const subject = 'Server Logs';
+    const htmlBody = `
+      <h3>NOC - System Logs</h3>
+      <p>See Attached Files</p>
+    `;
+
+    const attachments: Attachment[] = [
+      { filename: 'logs-all.log', path: './logs/logs-all.log' },
+      { filename: 'logs-high.log', path: './logs/logs-high.log' },
+      { filename: 'logs-medium.log', path: './logs/logs-medium.log' },
+    ];
+
+    return this.sendEmail({
+      to,
+      subject,
+      attachments,
+      htmlBody,
+    });
   }
 }
--- 05-NOC-App/src/presentation/server.ts
+++ 05-NOC-App/src/presentation/server.ts
@@ -1,27 +1,35 @@
-import { CheckService } from '../domain/use-cases/checks/check-service';
 import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
 import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository.implementation';
 import { CronService } from './cron/cron-service';
+import { CheckService } from '../domain/use-cases/checks/check-service';
 import { EmailService } from './email/email.service';
+import { SendEmailLogs } from '../domain/use-cases/email/send-email-logs';
 
 const fileSystemLogRepository = new LogRepositoryImpl(
   new FileSystemDatasource()
 );
+const emailService = new EmailService();
 
 export class Server {
   public static start() {
     console.log('Server started...');
 
     //Mandar email
-    const emailService = new EmailService();
-    emailService.sendEmail({
-      to: 'yusepah@gmail.com',
-      subject: 'Logs de Sistema',
-      htmlBody: `
-        <h3>Logs de sistema - NOC</h3>
-        <p>Ver logs adjuntos</p>
-      `,
-    });
+    // const emailService = new EmailService();
+    // emailService.sendEmail({
+    //   to: 'yusepah@gmail.com',
+    //   subject: 'Logs de Sistema',
+    //   htmlBody: `
+    //     <h3>Logs de sistema - NOC</h3>
+    //     <p>Ver logs adjuntos</p>
+    //   `,
+    // });
+
+    // Mandar mail con system logs
+    // emailService.sendEmailWithFileSystemLogs('yusepah@gmail.com');
+    new SendEmailLogs(emailService, fileSystemLogRepository).execute(
+      'yusepah@gmail.com'
+    );
 
     // CronService.createJob('*/5 * * * * *', () => {
     //   const url = 'http://localhost:3000';
