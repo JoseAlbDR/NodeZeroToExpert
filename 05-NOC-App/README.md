055d0ef435cf786f740682e6748aa236963ce101
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-02 11:11:30 +0100

    feat: add readme

---
+++ 05-NOC-App/README.md
@@ -0,0 +1,13 @@
+# dev
+
+1. Clone file env.template to .env
+2. Config environment variables
+
+```
+PORT=3000
+
+MAILER_EMAIL=
+MAILER_SECRET_KEY=
+
+PROD=false
+```
--- 05-NOC-App/src/app.ts
+++ 05-NOC-App/src/app.ts
@@ -1,12 +1,11 @@
-import { envs } from './config/plugins/env.plugin';
+// import { envs } from './config/plugins/env.plugin';
 import { Server } from './presentation/server';
-import 'dotenv/config';
 
 (async () => {
   main();
 })();
 
 function main() {
-  // Server.start();
-  console.log(envs.PORT);
+  Server.start();
+  // console.log(envs.PORT);
 }
