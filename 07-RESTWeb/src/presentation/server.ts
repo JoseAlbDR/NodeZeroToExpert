8bf486591c68d0e669ef5f777acaa0e424de12c5
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-04 23:12:09 +0100

    feat: server react SPA

--- 07-RESTWeb/src/presentation/server.ts
+++ 07-RESTWeb/src/presentation/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 
 export class Server {
   private app = express();
@@ -8,6 +9,10 @@ export class Server {
 
     //* Public Folder
     this.app.use(express.static('public'));
+
+    this.app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../../public/index.html'));
+    });
 
     this.app.listen(3000, () => {
       console.log(`Server running on port ${3000}`);
