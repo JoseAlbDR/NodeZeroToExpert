b8b7525ab1a613b24c66661eedd7006007322a06
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-04 23:20:50 +0100

    feat: create options interface

--- 07-RESTWeb/src/presentation/server.ts
+++ 07-RESTWeb/src/presentation/server.ts
@@ -1,23 +1,27 @@
 import express from 'express';
 import path from 'path';
 
+interface Options {
+  PORT: number;
+  PUBLIC_PATH: string;
+}
 export class Server {
   private app = express();
 
-  constructor(public port: number, public publicPath: string) {}
+  constructor(private readonly options: Options) {}
 
   async start() {
     //* Middlewares
 
     //* Public Folder
-    this.app.use(express.static(this.publicPath));
+    this.app.use(express.static(this.options.PUBLIC_PATH));
 
     this.app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../../public/index.html'));
     });
 
-    this.app.listen(this.port, () => {
-      console.log(`Server running on port ${this.port}`);
+    this.app.listen(this.options.PORT, () => {
+      console.log(`Server running on port ${this.options.PORT}`);
     });
   }
 }
