366965fe7ebd17273e88bf35a4e13c71053b4ac3
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-04 22:05:33 +0100

    feat: all test done

--- 05-NOC-App/src/presentation/email/email.service.test.ts
+++ 05-NOC-App/src/presentation/email/email.service.test.ts
@@ -1,17 +1,62 @@
-// import { SendMailOptions } from './email.service';
-// import { EmailService } from './email.service';
-// describe('presentation/email/email.service.ts', () => {
-//   test('should send email', async () => {
-//     const emailService = new EmailService();
+import { SendMailOptions } from './email.service';
+import { EmailService } from './email.service';
+import nodemailer from 'nodemailer';
+describe('presentation/email/email.service.ts', () => {
+  const mockSendMail = jest.fn();
 
-//     const options: SendMailOptions = {
-//       to: 'yusepah@gmail.com',
-//       subject: 'Test',
-//       htmlBody: '<h1>Test</h1>',
-//     };
+  nodemailer.createTransport = jest
+    .fn()
+    .mockReturnValue({ sendMail: mockSendMail });
 
-//     const emailSent = await emailService.sendEmail(options);
+  test('should send email', async () => {
+    const emailService = new EmailService();
 
-//     console.log(emailSent);
-//   });
-// });
+    const options: SendMailOptions = {
+      to: 'yusepah@gmail.com',
+      subject: 'Test',
+      htmlBody: '<h1>Test</h1>',
+    };
+
+    const emailSent = await emailService.sendEmail(options);
+
+    expect(emailSent).toBe(true);
+
+    expect(mockSendMail).toHaveBeenCalledWith({
+      attachments: expect.any(Array),
+      html: '<h1>Test</h1>',
+      subject: 'Test',
+      to: 'yusepah@gmail.com',
+    });
+  });
+
+  test('should send email with attachments', async () => {
+    const emailService = new EmailService();
+    await emailService.sendEmailWithFileSystemLogs('yusepah@gmail.com');
+
+    expect(mockSendMail).toHaveBeenCalledWith({
+      html: expect.any(String),
+      subject: 'Server Logs',
+      to: 'yusepah@gmail.com',
+      attachments: expect.arrayContaining([
+        { filename: 'logs-all.log', path: './logs/logs-all.log' },
+        { filename: 'logs-high.log', path: './logs/logs-high.log' },
+        { filename: 'logs-medium.log', path: './logs/logs-medium.log' },
+      ]),
+    });
+  });
+
+  test('should return false if an error occurred', async () => {
+    const emailService = new EmailService();
+    emailService.sendEmail = jest.fn().mockReturnValue(false);
+    const options: SendMailOptions = {
+      to: 'yusepah@gmail.com',
+      subject: 'Test',
+      htmlBody: '<h1>Test</h1>',
+    };
+
+    const emailSent = await emailService.sendEmail(options);
+
+    expect(emailSent).toBe(false);
+    expect(emailService.sendEmail).toHaveBeenCalledWith(options);
+  });
+});
