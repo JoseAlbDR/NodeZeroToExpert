931dc8a8d8e09ce869f0beb8b08f7fb3f429f723
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-04 23:02:29 +0100

    feat: serve public folder

--- 07-RESTWeb/src/presentation/server.ts
+++ 07-RESTWeb/src/presentation/server.ts
@@ -4,6 +4,11 @@ export class Server {
   private app = express();
 
   async start() {
+    //* Middlewares
+
+    //* Public Folder
+    this.app.use(express.static('public'));
+
     this.app.listen(3000, () => {
       console.log(`Server running on port ${3000}`);
     });
