61b46e7bba4b4aef1414feea21a511bdecbdd8e1
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-04 11:26:50 +0100

    feat: check-service tests

---
+++ 05-NOC-App/src/domain/use-cases/checks/check-service.test.ts
@@ -0,0 +1,52 @@
+import { LogEntity } from '../../entities/log.entity';
+import { CheckService } from './check-service';
+
+describe('domain/use-cases/checks/check-service.ts', () => {
+  const mockRepository = {
+    saveLog: jest.fn(),
+    getLogs: jest.fn(),
+  };
+
+  const successCallback = jest.fn();
+  const errorCallback = jest.fn();
+
+  const checkService = new CheckService(
+    mockRepository,
+    successCallback,
+    errorCallback
+  );
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('should call successCallback when fetch returns true', async () => {
+    const wasOk = await checkService.execute('https://google.com');
+
+    expect(wasOk).toBe(true);
+    expect(successCallback).toHaveBeenCalled();
+    expect(errorCallback).not.toHaveBeenCalled();
+
+    expect(mockRepository.saveLog).toBeCalledWith(expect.any(LogEntity));
+  });
+
+  test('should call errorCallback when fetch returns false', async () => {
+    const wasOk = await checkService.execute('https://googlessef.com');
+
+    expect(wasOk).toBe(false);
+    expect(errorCallback).toHaveBeenCalled();
+    expect(successCallback).not.toHaveBeenCalled();
+
+    expect(mockRepository.saveLog).toBeCalledWith(expect.any(LogEntity));
+  });
+
+  test('should return an error', async () => {
+    const fetchMock = jest.fn();
+    global.fetch = fetchMock;
+    const mockedErrorResponse = { ok: false };
+    const mockedUrl = 'iuyfiuyfiuyg';
+    fetchMock.mockResolvedValueOnce(mockedErrorResponse);
+    const wasOk = await checkService.execute(mockedUrl);
+    expect(wasOk).toBe(false);
+  });
+});
