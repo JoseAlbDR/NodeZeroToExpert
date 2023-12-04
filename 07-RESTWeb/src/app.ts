c7a831e4283ef82543c708a78d13b90e727debb8
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-04 23:24:43 +0100

    feat: more ordered form to pass port and publicPath to server

--- 07-RESTWeb/src/app.ts
+++ 07-RESTWeb/src/app.ts
@@ -8,7 +8,7 @@ import { Server } from './presentation/server';
 function main() {
   const { PORT, PUBLIC_PATH } = envs;
 
-  const server = new Server({ PORT, PUBLIC_PATH });
+  const server = new Server({ port: PORT, publicPath: PUBLIC_PATH });
 
   server.start();
 }
--- 07-RESTWeb/src/presentation/server.ts
+++ 07-RESTWeb/src/presentation/server.ts
@@ -2,26 +2,32 @@ import express from 'express';
 import path from 'path';
 
 interface Options {
-  PORT: number;
-  PUBLIC_PATH: string;
+  port: number;
+  publicPath: string;
 }
 export class Server {
   private app = express();
+  private readonly port: number;
+  private readonly publicPath: string;
 
-  constructor(private readonly options: Options) {}
+  constructor(options: Options) {
+    const { port, publicPath } = options;
+    this.port = port;
+    this.publicPath = publicPath;
+  }
 
   async start() {
     //* Middlewares
 
     //* Public Folder
-    this.app.use(express.static(this.options.PUBLIC_PATH));
+    this.app.use(express.static(this.publicPath));
 
     this.app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../../public/index.html'));
     });
 
-    this.app.listen(this.options.PORT, () => {
-      console.log(`Server running on port ${this.options.PORT}`);
+    this.app.listen(this.port, () => {
+      console.log(`Server running on port ${this.port}`);
     });
   }
 }
