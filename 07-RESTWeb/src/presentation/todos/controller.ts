edf87062f11d17cbad6599c76222f5a06ba98f29
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-05 13:02:05 +0100

    feat: delete todo controller and route

--- 07-RESTWeb/src/presentation/todos/controller.ts
+++ 07-RESTWeb/src/presentation/todos/controller.ts
@@ -33,9 +33,9 @@ export class TodosController {
     if (isNaN(id))
       return res.status(Status.BAD_REQUEST).json({ msg: 'Invalid id' });
 
-    const todo = todos.filter((todo) => todo.id === id);
+    const todo = todos.find((todo) => todo.id === id);
 
-    if (todo.length === 0)
+    if (!todo)
       return res
         .status(Status.NOT_FOUND)
         .json({ msg: `Todo with id ${id} not found` });
@@ -60,5 +60,45 @@ export class TodosController {
     todos.push(newTodo);
 
     res.status(Status.CREATED).json(newTodo);
+  };
+
+  public updateTodo = (req: Request, res: Response) => {
+    const id = +req.params.id;
+
+    if (isNaN(id))
+      return res.status(Status.BAD_REQUEST).json({ msg: 'Invalid id' });
+
+    const todo = todos.find((todo) => todo.id === id);
+
+    if (!todo)
+      return res
+        .status(Status.NOT_FOUND)
+        .json({ msg: `Todo with id ${id} not found` });
+
+    const { text } = req.body;
+    if (!text)
+      return res
+        .status(Status.BAD_REQUEST)
+        .json({ msg: 'Property text is required' });
+
+    todo.text = text;
+
+    res.status(Status.OK).json({ updatedTodo: todo });
+  };
+
+  public deleteTodo = (req: Request, res: Response) => {
+    const id = +req.params.id;
+
+    const todoIndex = todos.findIndex((todo) => todo.id === id);
+
+    if (todoIndex < 0)
+      return res
+        .status(Status.NOT_FOUND)
+        .json({ msg: `Todo with id: ${id} not found` });
+
+    const deletedTodo = todos[todoIndex];
+    todos.splice(todoIndex, 1);
+
+    res.status(Status.OK).json({ deletedTodo });
   };
 }
