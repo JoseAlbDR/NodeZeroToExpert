c4c82598ca489af7d339685e371f4a9fef539deb
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-05 22:31:03 +0100

    feat: TodoDatasource and TodoRepository

---
+++ 07-RESTWeb/src/domain/repositories/todo.repository.ts
@@ -0,0 +1,13 @@
+import { TodoEntity } from '../entities/todo.entity';
+import { CreateTodoDto } from '../dtos/todos/create-todo.dto';
+import { UpdateTodoDto } from '../dtos';
+
+export abstract class TodoRepository {
+  abstract create(createTodoDto: CreateTodoDto): Promise<TodoEntity>;
+
+  //todo: pagination
+  abstract getAll(): Promise<TodoEntity[]>;
+  abstract getById(id: number): Promise<TodoEntity>;
+  abstract updateById(updateTodoDto: UpdateTodoDto): Promise<TodoEntity>;
+  abstract deleteById(id: number): Promise<TodoEntity>;
+}
