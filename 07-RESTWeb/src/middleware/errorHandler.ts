801296079f876f1cbabdb98b40aa62f498f44f2f
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-05 17:20:24 +0100

    feat: errorHandler middleware

---
+++ 07-RESTWeb/src/middleware/errorHandler.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from 'express';
+import { Status } from '../config/plugins/statusCodes';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+
+export class ErrorHandler {
+  public static middleware = (
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    console.log(err);
+
+    let msg, statusCode;
+
+    if (err instanceof PrismaClientKnownRequestError && err.code === 'P2025') {
+      statusCode = Status.NOT_FOUND;
+      msg = err.meta?.cause;
+    }
+
+    return res.status(statusCode || Status.INTERNAL_SERVER_ERROR).json({
+      name: err.name,
+      msg: msg || err.message || 'Internal Server Error. Try Again Later',
+      stack: err?.stack,
+    });
+  };
+}
