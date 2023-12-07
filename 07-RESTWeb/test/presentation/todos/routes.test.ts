432d5709cce8dc7db790dc4d6c6bd3a8bf8f398e
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2023-12-07 17:04:09 +0100

    feat: getTodos tested

--- 07-RESTWeb/test/presentation/todos/routes.test.ts
+++ 07-RESTWeb/test/presentation/todos/routes.test.ts
@@ -7,13 +7,34 @@ describe('Todo route testing', () => {
     await testServer.start();
   });
 
-  afterAll(() => testServer.close());
+  afterAll(() => {
+    testServer.close();
+  });
+
+  const todo1 = { text: 'Hello there 1' };
+  const todo2 = { text: 'Hello there 2' };
 
   test('should return TODOs api/todo', async () => {
+    await prisma.todo.deleteMany();
+    await prisma.todo.createMany({ data: [todo1, todo2] });
+
     const response = await request(testServer.app)
       .get('/api/v1/todos')
       .expect(200);
 
-    console.log(response.body);
+    const body = response.body;
+
+    expect(body).toBeInstanceOf(Array);
+    expect(body.length).toBe(2);
+    expect(body[0].text).toBe(todo1.text);
+    expect(body[1].text).toBe(todo2.text);
+
+    // expect(body[0]).toEqual(
+    //   expect.objectContaining({
+    //     id: expect.any(Number),
+    //     text: expect.any(String),
+    //     completedAt: expect.any(null),
+    //   })
+    // );
   });
 });
