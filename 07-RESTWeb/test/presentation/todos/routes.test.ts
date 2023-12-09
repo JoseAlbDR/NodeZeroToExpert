ae9e30c6f86d3c5786b55c732ef4a00c6185e28c
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2023-12-09 11:37:32 +0100

    feat: test /api/v1/todos/:id

--- 07-RESTWeb/test/presentation/todos/routes.test.ts
+++ 07-RESTWeb/test/presentation/todos/routes.test.ts
@@ -11,6 +11,10 @@ describe('Todo route testing', () => {
     testServer.close();
   });
 
+  beforeEach(async () => {
+    await prisma.todo.deleteMany();
+  });
+
   const todo1 = { text: 'Hello there 1' };
   const todo2 = { text: 'Hello there 2' };
 
@@ -24,10 +28,13 @@ describe('Todo route testing', () => {
 
     const body = response.body;
 
+    console.log(body);
+
     expect(body).toBeInstanceOf(Array);
     expect(body.length).toBe(2);
     expect(body[0].text).toBe(todo1.text);
     expect(body[1].text).toBe(todo2.text);
+    expect(body[0].completedAt).toBeNull();
 
     // expect(body[0]).toEqual(
     //   expect.objectContaining({
@@ -36,5 +43,25 @@ describe('Todo route testing', () => {
     //     completedAt: expect.any(null),
     //   })
     // );
+  });
+
+  test('should return a TODO api/todos/:id', async () => {
+    await prisma.todo.deleteMany();
+
+    const todo1 = { text: 'Hello there 1' };
+
+    const todo = await prisma.todo.create({
+      data: todo1,
+    });
+
+    console.log(todo);
+
+    expect(todo.text).toEqual('Hello there 1');
+    expect(todo.completedAt).toBeNull();
+    expect(todo).toEqual({
+      id: expect.any(Number),
+      text: expect.any(String),
+      completedAt: null,
+    });
   });
 });
