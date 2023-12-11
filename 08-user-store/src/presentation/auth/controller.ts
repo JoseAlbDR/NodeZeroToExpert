12c7d0ea77d154b0fc499fbb62f5f8e68dbc7b99
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-11 17:00:00 +0100

    feat: auth controller and routes initial setup

---
+++ 08-user-store/src/presentation/auth/controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+
+export class AuthController {
+  constructor() {}
+
+  registerUser = async (req: Request, res: Response) => {
+    res.json('registerUser');
+  };
+
+  loginUser = async (req: Request, res: Response) => {
+    res.json('loginUser');
+  };
+
+  validateEmail = async (req: Request, res: Response) => {
+    res.json('validateEmail');
+  };
+}
---
+++ 08-user-store/src/presentation/auth/routes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import { AuthController } from './controller';
+
+export class AuthRoutes {
+  static get routes(): Router {
+    const router = Router();
+
+    const authController = new AuthController();
+
+    router.post('/login', authController.loginUser);
+    router.post('/register', authController.registerUser);
+
+    router.get('/validate-email/:token', authController.validateEmail);
+
+    return router;
+  }
+}
