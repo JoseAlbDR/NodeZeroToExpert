d37a273c33b7fc31bca9195b2bf5462057b184d0
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-12 11:47:50 +0100

    feat: register user with hashed password

--- 08-user-store/src/presentation/services/auth.service.ts
+++ 08-user-store/src/presentation/services/auth.service.ts
@@ -1,3 +1,4 @@
+import { bcryptAdapter } from '../../config';
 import { UserModel } from '../../data';
 import { CustomError, RegisterUserDto, UserEntity } from '../../domain';
 
@@ -13,10 +14,9 @@ export class AuthService {
     try {
       const user = new UserModel(registerUserDto);
 
-      await user.save();
-
       // Encrypt password
-
+      user.password = bcryptAdapter.hash(registerUserDto.password);
+      await user.save();
       // JWT
 
       // Confirmation email
