9e36f2161faffc12ebb832b61fccf66b14d6fc0b
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-12 17:33:30 +0100

    feat: JWT_SEED envs

--- 08-user-store/src/config/jwt.adapter.ts
+++ 08-user-store/src/config/jwt.adapter.ts
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import { envs } from './envs';
 
 export class JwtAdapter {
   static generateToken = (
@@ -6,10 +7,15 @@ export class JwtAdapter {
     duration: string = '2h'
   ) => {
     return new Promise((resolve) => {
-      jwt.sign(payload, 'SEED', { expiresIn: duration }, (err, token) => {
-        if (err) return resolve(null);
-        resolve(token);
-      });
+      jwt.sign(
+        payload,
+        envs.JWT_SEED,
+        { expiresIn: duration },
+        (err, token) => {
+          if (err) return resolve(null);
+          resolve(token);
+        }
+      );
     });
   };
 
