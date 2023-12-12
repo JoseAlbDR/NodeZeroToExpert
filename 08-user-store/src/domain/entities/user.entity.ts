a2cf831eb1774c97c29f30b364e463e5ce0539bf
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-12 11:04:44 +0100

    feat: UserEntity complete

--- 08-user-store/src/domain/entities/user.entity.ts
+++ 08-user-store/src/domain/entities/user.entity.ts
@@ -1,10 +1,38 @@
+import { CustomError } from '../errors/custom.error';
+
 export class UserEntity {
   constructor(
+    public readonly id: string,
     public readonly name: string,
     public readonly email: string,
     public readonly emailValidated: boolean,
     public readonly password: string,
-    public readonly img: string,
-    public readonly role: string
+    public readonly role: string[],
+    public readonly img?: string
   ) {}
+
+  static fromObject(obj: { [key: string]: any }) {
+    const { id, _id, name, email, emailValidated, password, role, img } = obj;
+
+    if (!_id && !id) {
+      throw CustomError.badRequest('Missing id');
+    }
+
+    if (!name) throw CustomError.badRequest('Missing name');
+    if (!email) throw CustomError.badRequest('Missing email');
+    if (emailValidated === undefined)
+      throw CustomError.badRequest('Missing emailValidation');
+    if (!password) throw CustomError.badRequest('Missing password');
+    if (!role) throw CustomError.badRequest('Missing role');
+
+    return new UserEntity(
+      _id || id,
+      name,
+      email,
+      emailValidated,
+      password,
+      role,
+      img
+    );
+  }
 }
