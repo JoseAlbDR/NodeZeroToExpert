20f2dd4fd4a49a22a4c664d23dc51d344953be83
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-13 11:52:58 +0100

    feat: CategoryModel

---
+++ 08-user-store/src/data/mongo/models/category.model.ts
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from 'mongoose';
+import { bcryptAdapter } from '../../../config';
+
+const CategorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'name is required'],
+  },
+  available: {
+    type: Boolean,
+    default: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
+
+export const CategoryModel = mongoose.model('Category', CategorySchema);
