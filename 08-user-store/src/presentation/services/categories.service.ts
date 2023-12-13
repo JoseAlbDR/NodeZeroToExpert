db363ca93f0b46fe9f8bb49620359f2d15c25707
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-13 18:36:30 +0100

    feat: pagination with currentPage, maxPages, limit, total, next and prev

--- 08-user-store/src/presentation/services/categories.service.ts
+++ 08-user-store/src/presentation/services/categories.service.ts
@@ -11,15 +11,39 @@ export class CategoriesService {
     const { limit, page } = paginationDto;
 
     try {
-      const categories = await CategoryModel.find()
-        .skip((page - 1) * limit)
-        .limit(limit);
+      const [total, categories] = await Promise.all([
+        CategoryModel.countDocuments(),
+        CategoryModel.find()
+          .skip((page - 1) * limit)
+          .limit(limit),
+      ]);
+
+      // const total = await CategoryModel.countDocuments();
+      // const categories = await CategoryModel.find()
+      //   .skip((page - 1) * limit)
+      //   .limit(limit);
+
+      const maxPages = Math.ceil(total / limit);
 
       const categoryEntities = categories.map((category) =>
         CategoryEntity.fromObject(category)
       );
 
-      return categoryEntities;
+      return {
+        currentPage: page,
+        maxPages,
+        limit: limit,
+        total,
+        next:
+          page + 1 <= maxPages
+            ? `/api/v1/categories?page=${page + 1}&limit=${limit}`
+            : null,
+        prev:
+          page - 1 > 0
+            ? `/api/v1/categories?page=${page - 1}&limit=${limit}`
+            : null,
+        categories: categoryEntities,
+      };
     } catch (error) {
       console.log(error);
       throw CustomError.internalServer('Internal Server Error');
