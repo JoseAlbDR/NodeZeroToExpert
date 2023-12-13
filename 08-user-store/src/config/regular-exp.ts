0c3073b1d8f55add21e18960fa311fcc1c84874c
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-13 12:51:18 +0100

    feat: check available in CreateCategoryDto

--- 08-user-store/src/config/regular-exp.ts
+++ 08-user-store/src/config/regular-exp.ts
@@ -2,4 +2,5 @@ export const regularExps = {
   // email
   email: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
   password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).{6,}$/,
+  mongoId: /^[0-9a-fA-F]{24}$/,
 };
--- 08-user-store/src/domain/dtos/category/create.category.dto.ts
+++ 08-user-store/src/domain/dtos/category/create.category.dto.ts
@@ -1,18 +1,25 @@
+import { regularExps } from '../../../config';
 import { UserEntity } from '../../entities/user.entity';
 
 export class CreateCategoryDto {
   private constructor(
     public readonly name: string,
     public readonly available: boolean,
-    public readonly user: UserEntity
+    public readonly user: string
   ) {}
 
   static create(object: { [key: string]: any }): [string?, CreateCategoryDto?] {
-    const { name, available, user } = object;
+    const { name, available = false, user } = object;
+
+    let availableBoolean = available;
 
     if (!name || name === '') return ['Mising name'];
-    if (!user || !(user instanceof UserEntity)) return ['Mismatching user'];
+    if (!user) return ['Missing user'];
+    if (typeof available !== 'boolean') {
+      availableBoolean = available === 'true';
+    }
+    if (!regularExps.mongoId.test(user)) return ['User is not a valid mongoId'];
 
-    return [undefined, new CreateCategoryDto(name, available, user)];
+    return [undefined, new CreateCategoryDto(name, availableBoolean, user)];
   }
 }
