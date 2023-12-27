27888f62bbbc1c100e23264888c7ddc6343cb8d4
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-27 18:22:27 +0100

    feat: get x-github-event header

--- 09-github-webhooks/src/presentation/github/controller.ts
+++ 09-github-webhooks/src/presentation/github/controller.ts
@@ -4,9 +4,11 @@ export class GithubController {
   constructor() {}
 
   webhookHandler = (req: Request, res: Response) => {
+    const githubEvent = req.header('x-github-event') ?? 'unknown';
     const payload = req.body;
 
     console.log(payload);
+    console.log(githubEvent);
 
     res.status(202).send('Accepted');
   };
