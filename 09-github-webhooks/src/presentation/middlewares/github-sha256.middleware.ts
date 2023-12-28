5fb60e612c38cde941c02e5bd568fa2d78171c6f
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-28 19:36:30 +0100

    feat: try catch in verify function inside webhook auth middleware

--- 09-github-webhooks/src/presentation/middlewares/github-sha256.middleware.ts
+++ 09-github-webhooks/src/presentation/middlewares/github-sha256.middleware.ts
@@ -4,18 +4,21 @@ import { envs } from '../../config';
 
 export class GithubSha256Middleware {
   private static verify = (req: Request) => {
-    const signature = crypto
-      .createHmac('sha256', envs.SECRET_TOKEN)
-      .update(JSON.stringify(req.body))
-      .digest('hex');
+    try {
+      const signature = crypto
+        .createHmac('sha256', envs.SECRET_TOKEN)
+        .update(JSON.stringify(req.body))
+        .digest('hex');
 
-    const xHubSignature = req.header('x-hub-signature-256') ?? '';
+      const xHubSignature = req.header('x-hub-signature-256') ?? '';
 
-    if (!xHubSignature) return false;
-
-    let trusted = Buffer.from(`sha256=${signature}`, 'ascii');
-    let untrusted = Buffer.from(xHubSignature, 'ascii');
-    return crypto.timingSafeEqual(trusted, untrusted);
+      let trusted = Buffer.from(`sha256=${signature}`, 'ascii');
+      let untrusted = Buffer.from(xHubSignature, 'ascii');
+      return crypto.timingSafeEqual(trusted, untrusted);
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   };
 
   static verifySignature = (
