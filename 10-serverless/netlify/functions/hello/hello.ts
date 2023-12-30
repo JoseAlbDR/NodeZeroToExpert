d8a0301740a664747bc2446343e82c8bb3ea860c
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-30 11:36:26 +0100

    feat: add netlify dev script

--- 10-serverless/netlify/functions/hello/hello.ts
+++ 10-serverless/netlify/functions/hello/hello.ts
@@ -6,5 +6,11 @@ export const handler: Handler = async (
 ) => {
   return {
     statusCode: 200,
+    body: JSON.stringify({
+      message: 'Hello there',
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
   };
 };
