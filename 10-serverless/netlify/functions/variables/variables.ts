dd10e102abbc889c6203c942367d1a0573210ccf
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-30 11:42:32 +0100

    feat: new test function

--- 10-serverless/netlify/functions/variables/variables.ts
+++ 10-serverless/netlify/functions/variables/variables.ts
@@ -4,10 +4,14 @@ export const handler: Handler = async (
   event: HandlerEvent,
   context: HandlerContext
 ) => {
+  const myImportantVariable = process.env.MY_IMPORTANT_VARIABLE;
+
+  if (!myImportantVariable) throw new Error('Missing MY_IMPORTANT_VARIABLE');
+
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Hello there!',
+      myImportantVariable,
     }),
     headers: {
       'Content-Type': 'application/json',
