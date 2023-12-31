236f923af740472f27b06dca9a5c15d32c5a9d26
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-31 11:28:05 +0100

    feat: onStar and onIssue functions

--- 10-serverless/netlify/functions/github-discord/github-discord.ts
+++ 10-serverless/netlify/functions/github-discord/github-discord.ts
@@ -1,4 +1,6 @@
 import { Handler, HandlerContext, HandlerEvent } from '@netlify/functions';
+import { GithubStarPayload } from '../../../src/interfaces/github-start.interface';
+import { GithubIssuePayload } from '../../../src/interfaces/github-issue.interface';
 
 const notify = async (message: string) => {
   const body = {
@@ -26,11 +28,51 @@ const notify = async (message: string) => {
   return true;
 };
 
+const onStar = (payload: GithubStarPayload) => {
+  const { starred_at, action, sender, repository } = payload;
+
+  const message = `User ${sender.login} ${action} star on ${repository.full_name}`;
+
+  return message;
+};
+
+const onIssue = (payload: GithubIssuePayload) => {
+  const { action, issue } = payload;
+
+  if (action === 'opened')
+    return `An issue was opened by ${issue.user.login} with this title ${issue.title}`;
+
+  if (action === 'closed')
+    return `An issue was closed by ${issue.user.login} with this title ${issue.title}`;
+
+  if (action === 'reopened')
+    return `An issue was reopened by ${issue.user.login} with this title ${issue.title}`;
+
+  return `Unhandled action for the issue event ${action}`;
+};
+
 export const handler: Handler = async (
   event: HandlerEvent,
   context: HandlerContext
 ) => {
-  await notify('Hello world from netlify functions');
+  const githubEvent = event.headers['x-github-event'] ?? 'unknown';
+  // const signature = req.header('x-hub-signature-256') ?? 'unknown';
+  const payload = JSON.parse(event.body ?? '{}');
+
+  let message = '';
+
+  switch (githubEvent) {
+    case 'star':
+      message = onStar(payload);
+      break;
+    case 'issues':
+      message = onIssue(payload);
+      break;
+    default:
+      console.log('Unknown event: ' + githubEvent);
+  }
+
+  await notify(message);
 
   return {
     statusCode: 200,
