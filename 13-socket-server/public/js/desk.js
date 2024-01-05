5c331838cfb26889964c858ea46cbba798649282
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-01-05 12:29:08 +0100

    feat: loadInitialCount function in front end desk

--- 13-socket-server/public/js/desk.js
+++ 13-socket-server/public/js/desk.js
@@ -1,3 +1,12 @@
-
-
 console.log('Escritorio HTML');
+
+const pendingLbl = document.querySelector('#lbl-pending');
+
+const loadInitialCount = async () => {
+  const response = await fetch('/api/v1/ticket/pending');
+  const { tickets } = await response.json();
+
+  pendingLbl.innerHTML = tickets.length || 0;
+};
+
+loadInitialCount();
