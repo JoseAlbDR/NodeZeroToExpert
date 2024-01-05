2a06ae8b84727153b350df9917ac3f21626f9b06
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-01-05 12:22:24 +0100

    feat: new-ticket script in front end

--- 13-socket-server/public/js/new-ticket.js
+++ 13-socket-server/public/js/new-ticket.js
@@ -1,3 +1,29 @@
+const button = document.querySelector('button');
+const lastTicket = document.querySelector('span');
 
+button.addEventListener('click', () => {
+  createTicket();
+});
 
-console.log('Nuevo Ticket HTML');
+const getLastTicket = async () => {
+  const response = await fetch('http://localhost:3000/api/v1/ticket/last');
+
+  const { number } = await response.json();
+
+  return number;
+};
+
+const createTicket = async () => {
+  const response = await fetch('http://localhost:3000/api/v1/ticket', {
+    method: 'POST',
+  });
+
+  await printTicket();
+};
+
+const printTicket = async () => {
+  const number = await getLastTicket();
+  lastTicket.innerText = `Last Ticket: ${number}`;
+};
+
+printTicket();
