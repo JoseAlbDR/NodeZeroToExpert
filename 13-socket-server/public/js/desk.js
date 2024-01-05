8bf164416fba023e0ad8eae9f25e1a0f2cf12033
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-01-05 12:30:20 +0100

    feat: connected to websocket

--- 13-socket-server/public/js/desk.js
+++ 13-socket-server/public/js/desk.js
@@ -9,4 +9,25 @@ const loadInitialCount = async () => {
   pendingLbl.innerHTML = tickets.length || 0;
 };
 
+function connectToWebSockets() {
+  const socket = new WebSocket('ws://localhost:3000/ws');
+
+  socket.onmessage = (event) => {
+    console.log(event.data);
+  };
+
+  socket.onclose = (event) => {
+    console.log('Connection closed');
+    setTimeout(() => {
+      console.log('retrying to connect');
+      connectToWebSockets();
+    }, 1500);
+  };
+
+  socket.onopen = (event) => {
+    console.log('Connected');
+  };
+}
+
+connectToWebSockets();
 loadInitialCount();
